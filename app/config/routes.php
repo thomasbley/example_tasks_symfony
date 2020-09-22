52c75c559e93ee61a81c85fd53aec99949083f85
Thomas Bley <thomas.bley@bringmeister.de>
2020-09-22 21:37:53 +0200

    added getTask, getTasks, deleteTask, updateTask

--- app/config/routes.php
+++ app/config/routes.php
@@ -4,7 +4,26 @@ use App\Controller\TasksController;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return function (RoutingConfigurator $routes) {
+    $routes->add('getTasks', '/v1/tasks')
+        ->controller([TasksController::class, 'getTasks'])
+        ->methods(['GET']);
+
+    $routes->add('getTask', '/v1/tasks/{taskId}')
+        ->controller([TasksController::class, 'getTask'])
+        ->requirements(['taskId' => '\d+'])
+        ->methods(['GET']);
+
     $routes->add('createTask', '/v1/tasks')
         ->controller([TasksController::class, 'createTask'])
         ->methods(['POST']);
+
+    $routes->add('updateTask', '/v1/tasks/{taskId}')
+        ->controller([TasksController::class, 'updateTask'])
+        ->requirements(['taskId' => '\d+'])
+        ->methods(['PUT']);
+
+    $routes->add('deleteTask', '/v1/tasks/{taskId}')
+        ->controller([TasksController::class, 'deleteTask'])
+        ->requirements(['taskId' => '\d+'])
+        ->methods(['DELETE']);
 };
--- app/src/Controller/TasksController.php
+++ app/src/Controller/TasksController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use App\Model\Task;
 
 class TasksController extends AbstractController
 {
@@ -21,6 +22,46 @@ class TasksController extends AbstractController
     {
         $this->repo = $repo;
         $this->serializer = $serializer;
+    }
+
+    public function getTask(Customer $customer, int $taskId): JsonResponse
+    {
+        $task = $this->repo->getTask($customer, $taskId);
+
+        if (empty($task)) {
+            throw new HttpException(Response::HTTP_NOT_FOUND, 'task not found');
+        }
+
+        $data = $this->serializer->serializeTask($task);
+
+        return $this->json($data, Response::HTTP_OK);
+    }
+
+    public function getTasks(Customer $customer, string $completed): JsonResponse
+    {
+        if (!empty($completed)) {
+            return $this->getCompletedTasks($customer);
+        }
+
+        return $this->getCurrentTasks($customer);
+    }
+
+    public function getCurrentTasks(Customer $customer): JsonResponse
+    {
+        $tasks = $this->repo->getCurrentTasks($customer);
+
+        $data = $this->serializer->serializeTasks($tasks);
+
+        return $this->json($data, Response::HTTP_OK);
+    }
+
+    public function getCompletedTasks(Customer $customer): JsonResponse
+    {
+        $tasks = $this->repo->getCompletedTasks($customer);
+
+        $data = $this->serializer->serializeTasks($tasks);
+
+        return $this->json($data, Response::HTTP_OK);
     }
 
     public function createTask(Customer $customer, string $title, string $duedate): JsonResponse
@@ -40,5 +81,44 @@ class TasksController extends AbstractController
         $data = $this->serializer->serializeTask($task);
 
         return $this->json($data, Response::HTTP_CREATED, ['Location' => $location]);
+    }
+
+    public function updateTask(Customer $customer, int $taskId, string $title, string $duedate, string $completed): JsonResponse
+    {
+        $task = new Task();
+        $task->id = $taskId;
+        $task->title = $title;
+        $task->duedate = $duedate;
+        $task->completed = (bool) $completed;
+
+        if (empty($task->title)) {
+            throw new HttpException(Response::HTTP_BAD_REQUEST, 'missing title');
+        }
+        if (!DateTime::createFromFormat('Y-m-d', $task->duedate)) {
+            throw new HttpException(Response::HTTP_BAD_REQUEST, 'invalid duedate');
+        }
+
+        if (!$this->repo->taskExists($customer, $task->id)) {
+            throw new HttpException(Response::HTTP_NOT_FOUND, 'task not found');
+        }
+
+        $this->repo->updateTask($task);
+
+        // TODO implement update notification
+
+        $data = $this->serializer->serializeTask($task);
+
+        return $this->json($data, Response::HTTP_OK);
+    }
+
+    public function deleteTask(Customer $customer, int $taskId): Response
+    {
+        if (!$this->repo->taskExists($customer, $taskId)) {
+            throw new HttpException(Response::HTTP_NOT_FOUND, 'task not found');
+        }
+
+        $this->repo->deleteTask($taskId);
+
+        return new Response('', Response::HTTP_NO_CONTENT);
     }
 }
